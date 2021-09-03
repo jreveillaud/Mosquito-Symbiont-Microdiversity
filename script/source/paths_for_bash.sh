4862d368d0a56182a701477726a361cc5116697b
Hans Schrieke <34349345+HansSCHR@users.noreply.github.com>
2021-09-03 16:58:20 +0200

    Add source file for bash scripts

---
+++ script/source/paths_for_bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH_MAIN="/Volumes/MY_PASSPORT/draft_final"
+PATH_DATA=$PATH_MAIN/data
+PATH_R1_R2_FILES=$PATH_MAIN/output/0_useful_files
+PATH_MERGED=$PATH_MAIN/output/0_merged
+PATH_MERGE_SCRIPT=$PATH_MAIN/script/0_Merge
