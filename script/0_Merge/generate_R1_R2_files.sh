d7eb23e456f5d277af9349c8b32cc3ebd6c454f8
Hans Schrieke <34349345+HansSCHR@users.noreply.github.com>
2021-12-06 10:50:07 +0100

    Update scripts and additional files

--- script/0_Merge/generate_R1_R2_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# load paths
-source /Volumes/MY_PASSPORT/draft_final/script/source/paths_for_bash.sh
-
-# export list of R1 and R2 sequences
-cd $PATH_DATA
-ls | grep "R1" > $PATH_R1_R2_FILES/0_R1.txt
-ls | grep "R2" > $PATH_R1_R2_FILES/0_R2.txt
